86607dbeb42a345517442e5a3c7aed17bdcca32a
Eliot Diaz <eliot.daz@gmail.com>
2017-05-22 22:37:28 -0400

    Deteccion generalizada para las 3 marcas. No detecta bien ni la Marca L, ni la Marca 5x5.

--- DDM.py
+++ DDM.py
@@ -1,29 +1,12 @@
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt
 
 MIN_MATCH_COUNT = 10
 FLANN_INDEX_KDTREE = 0
 
-img1 = cv2.imread('Marcas/Marca Kanji.png',0) # queryImage
-
-# Initiate SIFT detector
-sift = cv2.xfeatures2d.SIFT_create()
-
-# Find the keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(img1,None)
-
-index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-search_params = dict(checks = 50)
-
-flann = cv2.FlannBasedMatcher(index_params, search_params)
-
-cap = cv2.VideoCapture(0)
-while(cap.isOpened()):
-	ret, frame = cap.read()
-
-	kp2, des2 = sift.detectAndCompute(frame,None)
-	matches = flann.knnMatch(des1,des2,k=2)
+def matchAndFind(imgQuery, kpQuery, desQuery, imgTrain, kpTrain, desTrain):
+	detect = False
+	matches = flann.knnMatch(desQuery,desTrain,k=2)
 
 	# Store all the good matches as per Lowe's ratio test.
 	good = []
@@ -33,29 +16,63 @@ while(cap.isOpened()):
 	print("Number of good matches: " + str(len(good)))
 
 	if len(good) > MIN_MATCH_COUNT:
-	    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
-	    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+		try:
+		    src_pts = np.float32([ kpQuery[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+		    dst_pts = np.float32([ kpTrain[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
 
-	    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
-	    matchesMask = mask.ravel().tolist()
+		    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+		    matchesMask = mask.ravel().tolist()
 
-	    h,w = img1.shape
-	    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-	    dst = cv2.perspectiveTransform(pts,M)
+		    h,w = imgQuery.shape
+		    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+		    dst = cv2.perspectiveTransform(pts,M)
 
-	    img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
-
+		    imgTrain = cv2.polylines(imgTrain,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+		    detect = True
+		except:
+			print("Exception ocurred.")		    
 	else:
-	    print "Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT)
-	    matchesMask = None
-
+		#print "Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT)
+		matchesMask = None
 	draw_params = dict(matchColor = (0,255,0), # draw matches in green color
 	                   singlePointColor = None,
 	                   matchesMask = matchesMask, # draw only inliers
 	                   flags = 2)
-
-	img3 = cv2.drawMatches(img1,kp1,frame,kp2,good,None,**draw_params)
+	img3 = cv2.drawMatches(imgQuery,kpQuery,imgTrain,kpTrain,good,None,**draw_params)
 	cv2.imshow("All", img3)
+	return detect
+
+imgTerraine = cv2.imread('Marcas/Marca Kanji.png',0) # Empty terraines zone marker 
+imgExchange = cv2.imread('Marcas/Marca L.png',0) # Exchange zone marker
+imgTank = cv2.imread('Marcas/Marca 5x5.png',0) # Milk tank marker
+
+# Initiate SIFT detector
+sift = cv2.xfeatures2d.SIFT_create()
+
+# Find the keypoints and descriptors with SIFT
+kpTerraine, desTerraine = sift.detectAndCompute(imgTerraine,None)
+kpExchange, desExchange = sift.detectAndCompute(imgExchange,None)
+kpTank, desTank = sift.detectAndCompute(imgTank,None)
+
+index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+search_params = dict(checks = 50)
+
+flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+cap = cv2.VideoCapture(0)
+while(cap.isOpened()):
+	ret, frame = cap.read()
+	kpFrame, desFrame = sift.detectAndCompute(frame,None)
+	if (matchAndFind(imgExchange, kpExchange, desExchange, frame, kpFrame, desFrame)):
+		print("Exchange zone DETECTED!")
+	elif (matchAndFind(imgTerraine, kpTerraine, desTerraine, frame, kpFrame, desFrame)):
+		print("Empty terraines zone DETECTED!")
+	elif (matchAndFind(imgTank, kpTank, desTank, frame, kpFrame, desFrame)):
+		print("Exchange zone DETECTED!")
+	else:
+		print("No zone detected.")
+
 	if cv2.waitKey(1) == ord("z"):
 		break
-cv2.destroyAllWindows()
+	
+	
