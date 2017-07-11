e42244d54e3ce647ff5f7f3e35b0adb69c6cade5
Arturo Toro <arturoantoniotr@gmail.com>
2017-07-11 11:51:17 -0400

    Create ServoPWM.py
    Utiliza la librería pigpio. Las instrucciones para la instalación están en:
    http://abyz.co.uk/rpi/pigpio/index.html
    
    El código esta basado en la información de las paginas:
    https://github.com/joan2937/pigpio/issues/67
    https://raspberrypi.stackexchange.com/questions/47439/how-to-turn-off-let-float-a-servo-attached-to-gpio
    
    Los motores con los que se realizaron las pruebas fueron:
    - SM-SR04 (Rotación continua): https://www.emoro.eu/shop/index.php?route=product/product&product_id=88
    - MG90S   (Ángulo) : https://engineering.tamu.edu/media/4247823/ds-servo-mg90s.pdf
    En el caso de los motores de angulo, el PWM define la orientación.
    
    NOTA IMPORTANTE: El número de pin NO es el de la Raspberry, es el número de GPIO. El GND de la raspberry
    DEBE estar común al GND de la alimentación del motor, en caso contrario el motor NO se mueve.

---
+++ MOTORS/ServoPWM.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import pigpio
+import time
+
+pi = pigpio.pi()
+pi.set_mode(4, pigpio.OUTPUT)
+
+print ("mode: ", pi.get_mode(4))
+print("setting to: ",pi.set_servo_pulsewidth(4, 1000)) // Mover servo
+print("set to: ",pi.get_servo_pulsewidth(4))
+
+time.sleep(1)
+
+print("setting to: ",pi.set_servo_pulsewidth(4, 1500)) // Detener servo (Hace torque)
+print("set to: ",pi.get_servo_pulsewidth(4))
+
+time.sleep(1)
+
+print("setting to: ",pi.set_servo_pulsewidth(4, 2000)) // Mover servo (Otra direccion)
+print("set to: ",pi.get_servo_pulsewidth(4))
+
+time.sleep(1)
+
+print("setting to: ",pi.set_servo_pulsewidth(4, 0))    // Detener servo (Sin torque)
+print("set to: ",pi.get_servo_pulsewidth(4))
+
+pi.stop()
