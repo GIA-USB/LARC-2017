ad2dbb7acc5c449efa183adcd895de0d3ff8dcbd
GIA <grupogiausb@gmail.com>
2017-09-14 23:55:43 +0400

    Se agragan los archivos para mover el robot en diversas direcciones

--- Motors/around.py
+++ Motors/around.py
@@ -9,12 +9,12 @@ pi.set_mode(26, pigpio.OUTPUT)
 pi.set_mode(16, pigpio.OUTPUT)
 pi.set_mode(20, pigpio.OUTPUT)
 
-pi.set_PWM_dutycycle(19, 255)
-pi.set_PWM_dutycycle(26, 0)
+pi.set_PWM_dutycycle(19, 0)
+pi.set_PWM_dutycycle(26, 255)
 
-pi.set_PWM_dutycycle(20, 255)
-pi.set_PWM_dutycycle(16, 0)
-time.sleep(5)
+pi.set_PWM_dutycycle(20, 0)
+pi.set_PWM_dutycycle(16, 125)
+time.sleep(10)
 
 pi.set_PWM_dutycycle(19, 0)
 pi.set_PWM_dutycycle(26, 0)
